165ea4de2c974605f97d0188b7a05932033ab3fa
Sujith-Rajan <sujithrajan223@gmail.com>
2024-04-29 12:43:22 +0530

    clinet headers update

--- frontend/app/lib/apiRequest.ts
+++ frontend/app/lib/apiRequest.ts
@@ -2,7 +2,12 @@ import axios from 'axios'
 
 const apiRequest = axios.create({
     baseURL:`${process.env.NEXT_PUBLIC_BASE_URL}/api`,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'https://mintleaves.onrender.com'
+    },
     withCredentials: true,
+    
 })
 
 export default apiRequest
