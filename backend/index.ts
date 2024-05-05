fd09a47a6adb7771b1948146406148405b6c4138
Sujith-Rajan <sujithrajan223@gmail.com>
2024-05-05 12:27:54 +0530

    modify

--- backend/index.ts
+++ backend/index.ts
@@ -22,33 +22,16 @@ const clinetUrl = process.env.CLIENT_URL as string
 const app: Express = express(); 
 const corsOption = {
   origin:clinetUrl,
-  methods:["GET","POST","PUT","DELETE"],
   credentials:true,
 }
 
 
 //////////////////////////// MIDDLEWEARS /////////////////////////////////
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin',clinetUrl);
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", 'true');
-//   res.setHeader("Access-Control-Allow-Private-Network", 'true');
-//   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-//   res.setHeader("Access-Control-Max-Age", 7200);
-//   next();
-// });
 
+app.use(cors(corsOption))
 app.use(initializeCookieSession())
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(cors(corsOption))
 app.use(express.json())
 app.use(cookieParser())
 
--- frontend/app/lib/apiRequest.ts
+++ frontend/app/lib/apiRequest.ts
@@ -2,10 +2,6 @@ import axios from 'axios'
 
 const apiRequest = axios.create({
     baseURL:`${process.env.NEXT_PUBLIC_BASE_URL}/api`,
-    // headers: {
-    //     'Content-Type': 'application/json',
-    //     'Access-Control-Allow-Origin':process.env.NEXT_PUBLIC_ORGIN_URL
-    // },
     withCredentials: true,
     
 })
